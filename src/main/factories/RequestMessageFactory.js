c081e4e008194b13da2e59730b6f9f43dd9579d9
Matthew Adams <matthew.adams@northscaler.com>
2020-09-21 10:25:50 -0700

    spread operator unnecessary in message.meta creation

--- src/main/factories/RequestMessageFactory.js
+++ src/main/factories/RequestMessageFactory.js
@@ -5,10 +5,7 @@ const AbstractMessageFactory = require('./AbstractMessageFactory')
 class RequestMessageFactory extends AbstractMessageFactory {
   create ({ action, data, traceId, correlationId } = {}) {
     const message = this._createBase({ data, traceId, correlationId })
-    message.meta = {
-      ...message.meta,
-      request: { action }
-    }
+    message.meta.request = { action }
 
     return message
   }
